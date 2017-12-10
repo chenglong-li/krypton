e138358f3192dc20e22e95a05239373a79e9306c
Jackie <longestar@hotmail.com>
2017-12-09 18:46:12 -0600

    [update] find wallet by userId

--- src/main/java/com/assignment/cryptocurrency/repository/WalletRepository.java
+++ src/main/java/com/assignment/cryptocurrency/repository/WalletRepository.java
@@ -1,8 +1,11 @@
 package com.assignment.cryptocurrency.repository;
 
 import com.assignment.cryptocurrency.model.entity.Wallet;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Created by Jackie on 05/12/2017.
@@ -10,4 +13,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "wallet_list", path = "wallets")
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
 
+  @RestResource
+  List<Wallet> findByUserId(@Param("userId") Integer userId);
 }
