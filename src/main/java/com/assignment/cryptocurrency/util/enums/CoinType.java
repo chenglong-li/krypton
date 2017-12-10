5c259b38ebcd2cc676f570102c778d80be82ccab
Jackie <longestar@hotmail.com>
2017-12-10 16:13:07 -0600

    [update] update coin type

--- src/main/java/com/assignment/cryptocurrency/util/enums/CoinType.java
+++ src/main/java/com/assignment/cryptocurrency/util/enums/CoinType.java
@@ -1,7 +1,7 @@
 package com.assignment.cryptocurrency.util.enums;
 
 public enum CoinType {
-  Bitcoin(0), Walton(1);
+  Bitcoin(0), Ethereum(1), Litecoin(2), Iota(3), Neo(4);
   private final Integer coinType;
 
   CoinType(Integer coinType) {
