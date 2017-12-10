2b781b4d9129be4a3ba4cb2f3eb53566ba0bbd2f
Poorya Piroozan <hellhammervs@gmail.com>
2017-12-09 21:57:14 -0800

    added the price and other stuff

--- src/main/java/com/assignment/cryptocurrency/UIController/WalletController.java
+++ src/main/java/com/assignment/cryptocurrency/UIController/WalletController.java
@@ -15,6 +15,8 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.json.JsonObject;
 import javax.validation.constraints.Null;
@@ -31,7 +33,7 @@ import com.assignment.cryptocurrency.util.Storage;
 
 import javafx.fxml.*;
 
-public class WalletController implements Initializable
+public class WalletController  implements Initializable
 {
 	//---------------------------------------------------------------------
 	
@@ -42,18 +44,26 @@ public class WalletController implements Initializable
 	@FXML private Button showDashboardAction;
 	
 	@FXML private Label walletAddress1;
-	@FXML private Label coin1;
+	@FXML private Label coinLbl1;
 	@FXML private Label amount1;
 	@FXML private Label walletAddress2;
-	@FXML private Label coin2;
+	@FXML private Label coinLbl2;
 	@FXML private Label amount2;
 	@FXML private Label walletAddress3;
-	@FXML private Label coin3;
+	@FXML private Label coinLbl3;
 	@FXML private Label amount3;
+	
+	@FXML private Label price1;
+	@FXML private Label price2;
+	
+	Timer t;
+	String coinLabel1, coinLabel2;
 	//---------------------------------------------------------------------
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
+		
+		
 		showDashboardAction.setOnAction(e ->
 		{
 			((Node) e.getSource()).getScene().getWindow().hide();
@@ -63,32 +73,12 @@ public class WalletController implements Initializable
 		StringBuilder builder= getUserWalletInfo();
 		if (builder!=null)
 		{
-			JSONObject jsonObj = new JSONObject(builder.toString());
-			System.out.println(jsonObj);
-			JSONArray arr = jsonObj.getJSONObject("_embedded").getJSONArray("wallet_list");
-			System.out.println(arr.length());
-			for (int i = 0; i < arr.length(); i++) 
-			{
-				JSONObject walletJson= arr.getJSONObject(i);
-				//scene.lookup("#history");
-				if (i==0)
-				{
-					
-					walletAddress1.setText(walletJson.getString("address"));
-					coin1.setText(Integer.toString(walletJson.getInt("coin_id")));
-					amount1.setText(Double.toString(walletJson.getDouble("amount")));
-				}
-				else if (i==1)
-				{
-					
-					walletAddress2.setText(walletJson.getString("address"));
-					coin2.setText(Integer.toString(walletJson.getInt("coin_id")));
-					amount2.setText(Double.toString(walletJson.getDouble("amount")));
-				}
-			}
+			fillLabelsFromStringBuilder(builder);
 			
 		}
-			
+		
+		
+		
 		Object firstName=Storage.getInstance().get("firstName");
 		Object lastName=Storage.getInstance().get("lastName");
 		if (firstName!=null)
@@ -96,11 +86,170 @@ public class WalletController implements Initializable
 		if (lastName!=null)
 			lastNameLbl.setText(lastName.toString());
 		
+		updatePrices();
+		
+		
+		 /*t = new Timer( );
+		t.scheduleAtFixedRate(new TimerTask() 
+		{
+
+		    @Override
+		    public void run() 
+		    {
+		      updatePrices();
+
+		    }
+		}, 1000,10000);
+		 
+		*/
+	}
+	//---------------------------------------------------------------------
+	void updatePrices()
+	{
+		//String coinName1=coinLbl1.getText();
+		
+		if (coinLabel1!="")//&& coinName1!=null)
+		{
+			//System.out.println("_"+coinLabel1);
+			String price1s= getCoinPriceByName(coinLabel1);
+			if (price1!=null)
+				price1.setText(price1s.toString());
+		}
+		
+		if (coinLabel2!="")//&& coinName1!=null)
+		{
+			//System.out.println("_"+coinLabel1);
+			String price2s= getCoinPriceByName(coinLabel2);
+			if (price2!=null)
+				price2.setText(price2s.toString());
+		}
+		/*String coinName2=coinLbl2.getText();
+		if (coinName1!="" && coinName2!=null)
+		{
+			String price2s= getCoinPriceByName(coinName2);
+			if (price2!=null)
+				price2.setText(price2s.toString());
+		}*/
+	}
+	//---------------------------------------------------------------------
+	String getCoinPriceByName(String name)
+	{
+		CloseableHttpClient httpClient=null;
+		try 
+		{
+			System.out.println("_"+name);
+			httpClient = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet("https://api.coinmarketcap.com/v1/ticker/"+name);
+		    request.addHeader("content-type", "application/json");
+		    HttpResponse  response = httpClient.execute(request);
+		    if(response.getStatusLine().getStatusCode()==200)
+		    {
+		    	BufferedReader reader = null;
+				try 
+				{
+					reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+					StringBuilder builder = new StringBuilder();
+					for (String line = null; (line = reader.readLine()) != null;) 
+					{
+					    builder.append(line).append("\n");
+					}
+					JSONArray arr = new JSONArray(builder.toString());
+					JSONObject json= arr.getJSONObject(0);
+					return json.getString("price_usd");
+				}
+				catch (Exception ex) 
+				{
+				} 
+				finally 
+				{
+					
+				}
+		    }
+		} 
+		catch (Exception ex) 
+		{
+		} 
+		finally 
+		{
+			try 
+			{
+				httpClient.close();
+			} 
+			catch (IOException e1) 
+			{
+			}
+		}
+		return null;
+	}
+	//---------------------------------------------------------------------
+	String getCoinName(int id)
+	{
+		return getCoinName(Integer.toString(id));
+	}
+	//---------------------------------------------------------------------
+	String getCoinName(String id)
+	{
+		String name="";
+		switch (id) {
+		case "1":
+			name="Bitcoin";
+			break;
+		case "2":
+			name="Ethereum";
+			break;
+		case "3":
+			name="Litecoin";
+			break;
+		case "4":
+			name="Iota";
+			break;
+		case "5":
+			name="Neo";
+			break;
+		default:
+			break;
+		}
+		return name;
+	}
+	//---------------------------------------------------------------------
+	void fillLabelsFromStringBuilder(StringBuilder builder)
+	{
+		JSONObject jsonObj = new JSONObject(builder.toString());
+		
+		JSONArray arr = jsonObj.getJSONObject("_embedded").getJSONArray("wallet_list");
+		
+		for (int i = 0; i < arr.length(); i++) 
+		{
+			
+			//scene.lookup("#history");
+			if (i==0)
+			{
+				JSONObject walletJson= arr.getJSONObject(0);
+				walletAddress1.setText(walletJson.getString("address"));
+				int coinId=walletJson.getInt("coin_id");
+				//System.out.println()
+				coinLbl1.setText(getCoinName(coinId));
+				coinLabel1=getCoinName(coinId);
+				amount1.setText(Double.toString(walletJson.getDouble("amount")));
+			}
+			else if (i==1)
+			{
+				JSONObject walletJson= arr.getJSONObject(1);
+				walletAddress2.setText(walletJson.getString("address"));
+				int coinId=walletJson.getInt("coin_id");
+				//System.out.println()
+				coinLbl2.setText(getCoinName(coinId));
+				coinLabel2=getCoinName(coinId);
+				amount2.setText(Double.toString(walletJson.getDouble("amount")));
+			}
+		}
 		
 	}
 	//---------------------------------------------------------------------
 	void showDashboard()
 	{
+		if (t!=null)
+			t.cancel();
 		Parent root = null;
 		try 
 		{
@@ -167,7 +316,7 @@ public class WalletController implements Initializable
 		}
 		return null;
 	}
-	//---------------------------------------------------------------------
+	
 }
 
 
